a301acb1ff9994e3dce4ebdce99e648541f5a659
yossi <yossi@delver.com>
2012-10-10 17:21:54 +0200

    Adding application configurations

---
+++ trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Domain/Configuration/ApplicationSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Configuration;
+
+namespace SocialInsight.Domain.Configuration
+{
+	public class ApplicationSettings
+	{
+		public long AppId { get { return Convert.ToInt64(ConfigurationManager.AppSettings["app:id"]); } }
+		public string AppSecret { get { return ConfigurationManager.AppSettings["app:secret"]; } }
+	}
+}
