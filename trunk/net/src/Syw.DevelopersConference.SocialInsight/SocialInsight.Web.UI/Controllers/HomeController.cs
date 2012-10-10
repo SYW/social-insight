243a991a507558b839a4009db7f3080cc79d9e43
yossi <yossi@delver.com>
2012-10-10 16:35:16 +0200

    Basic solution with an MVC project

---
+++ trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SocialInsight.Web.UI.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Dashboard()
+		{
+			return View();
+		}
+	}
+}
---
+++ trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Web.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialInsight.Web.UI.MvcApplication" Language="C#" %>
