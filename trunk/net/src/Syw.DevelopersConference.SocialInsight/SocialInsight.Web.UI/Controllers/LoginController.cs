47e5ef0214b578668aab9202c1ea92c8045cc8a0
yossi <yossi@delver.com>
2012-10-10 19:11:27 +0200

    Adding login canvas

---
+++ trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Web.UI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialInsight.Web.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+		[RequireHttps]
+        public ActionResult Index()
+        {
+            return View("~/Views/Login.cshtml");
+        }
+    }
+}
