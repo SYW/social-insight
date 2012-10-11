ff8e4a5596616d125cd07a462e83db00020de539
yossi <yossi@delver.com>
2012-10-11 09:58:21 +0200

    Adding a call to the platform API in order to get the user name and displaying it in the post login page

---
+++ trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Domain/Users/UsersApi.cs
@@ -0,0 +1,35 @@
+﻿using Platform.Client;
+using Platform.Client.Common.Context;
+using SocialInsight.Domain.Configuration;
+
+namespace SocialInsight.Domain.Users
+{
+	public interface IUsersApi
+	{
+		UserDto GetCurrentUser();
+	}
+
+	public class UsersApi : IUsersApi
+	{
+		private const string BasePath = "users";
+
+		private readonly PlatformProxy _proxy;
+		
+		public UsersApi()
+		{
+			_proxy = new PlatformProxy(new PlatformSettings(),
+			                           new ApplicationSettings(),
+			                           new PlatformTokenProvider(new HttpContextProvider()));
+		}
+
+		public UserDto GetCurrentUser()
+		{
+			return _proxy.Get<UserDto>(GetEndpointPath("current"));
+		}
+
+		private static string GetEndpointPath(string endpoint)
+		{
+			return string.Format("/{0}/{1}", BasePath, endpoint);
+		}
+	}
+}
--- trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Web.UI/Controllers/PostLoginController.cs
+++ trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Web.UI/Controllers/PostLoginController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Web.Mvc;
 using SocialInsight.Domain;
+using SocialInsight.Domain.Users;
 
 namespace SocialInsight.Web.UI.Controllers
 {
     public class PostLoginController : Controller
     {
 	    private readonly Routes _routes;
+	    private IUsersApi _usersApi; 
 
 	    public PostLoginController()
 	    {
 		    _routes = new Routes();
+			_usersApi = new UsersApi();
 	    }
 
 	    [RequireHttps]
@@ -17,7 +20,8 @@ namespace SocialInsight.Web.UI.Controllers
 	    {
 		    var model = new PostLoginModel
 			    {
-				    DashboardUrl = _routes.Dashboard()
+				    DashboardUrl = _routes.Dashboard(),
+					UserName = _usersApi.GetCurrentUser().Name
 			    };
 
             return View("~/Views/PostLogin.cshtml", model);
@@ -27,6 +31,7 @@ namespace SocialInsight.Web.UI.Controllers
 
 	public class PostLoginModel
 	{
+		public string UserName { get; set; }
 		public string DashboardUrl { get; set; }
 	}
 }
