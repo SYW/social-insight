b7069ea70d75ba4ce25c4c57a7aa884abbb4c65b
yossi <yossi@delver.com>
2012-10-12 11:40:51 +0200

    Added product insights repository which will store the pre-calculated score for each product

---
+++ trunk/net/src/Syw.DevelopersConference.SocialInsight/SocialInsight.Domain/Products/ProductsRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SocialInsight.Domain.DataAccess;
+
+namespace SocialInsight.Domain.Products
+{
+	public class ProductsRepository
+	{
+		private readonly SessionProvider _sessionProvider;
+
+		public ProductsRepository()
+		{
+			_sessionProvider = new SessionProvider();
+		}
+
+		public IList<ProductInsightDto> GetProductInsightForUser(long userId)
+		{
+			return _sessionProvider.Query<ProductInsightDto>(q => q.Where(x => x.UserId == userId));
+		}
+
+		public void SaveProductInsights(IList<ProductInsightDto> insights)
+		{
+			_sessionProvider.WithSession(s => s.SaveOrUpdate(insights));
+		}
+	}
+}
